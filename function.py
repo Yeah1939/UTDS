d6762a10d39975837ed730da3c9535145d6d39d7
Yeah1939 <cocacolatwink@gmail.com>
2025-05-17 13:32:57 +0300

    Update function.py

--- function.py
+++ function.py
@@ -15,3 +15,63 @@ class Enemy(pygame.Rect):
         self.image = self.image_list
         self.image_now = self.image
         self.image_count = 0
+    
+    #def move():
+
+class Maps(pygame.Rect):
+    def __init__(self,x,y,width,height,image):
+        super().__init__(x,y,width,height)
+        self.image = image
+
+    def make_map(self,maps):
+        x,y = 0,0
+        length = len(maps)
+        i = 0
+        j = 0
+        tmp = True
+        while tmp:
+            if i == length -1:
+                tmp = False
+            if j == len(maps[i]) -1:
+                j=0
+                i+= 1
+                x = 0
+                y += 20
+
+            if maps[i][j] == "1":
+                while tmp:
+                    print(temp_map,i,j)
+                    if i + 1 == length - 1 or j + 1 == len(maps[i]) - 1:
+                        tmp = False
+                    if maps[i+1][j] == "1":
+                        i += 1
+                        y += 20
+                        tmp = False
+                    elif maps[i][j+1] == "1":
+                        j += 1
+                        x += 20
+                    elif maps[i+1][j+1] == "1":
+                        j += 1
+                        i += 1
+                        y += 20
+                        x += 20
+                    elif maps[i-1][j+1] == "1":
+                        j += 1
+                        i -= 1
+                        y -= 20
+                        x += 20
+                        print(2)
+                    elif maps[i-1][j] == "1":
+                        i -= 1
+                        y -= 20
+                    elif maps[i][j-1] == "1":
+                        j -= 1
+                        x -= 20
+                    elif maps[i-1][j-1] == "1":
+                        j -= 1
+                        i -= 1
+                        y -= 20
+                        x -= 20
+                    temp_map.append((x,y))
+                    
+                    
